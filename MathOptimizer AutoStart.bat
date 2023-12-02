6274219d8942e7e164b93fad98a42b5aa6c248fe
worbadillitics <119168200+Worbnaticus@users.noreply.github.com>
2023-12-02 20:44:38 -0300

    AutoExecuter
    Final Edition autoexec archive.
    
    MathOptimizer project finished, it was an challeging code.
    You can change anithing in the code, open-source
    
    This is the only official mirror of MathOptimizer, anything other is fake and can be an trojan
    If you can't search for what is in the main archive of optimizer, don't execute it

---
+++ MathOptimizer AutoStart.bat
@@ -0,0 +1,110 @@
+@echo off
+
+set "ScriptName=%~nx0"
+set "NewScriptName=MathOptimizer AutoStart.bat"
+set "StartupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+rem Tamanho da tela
+
+mode con cols=91 lines=13
+
+rem Resolution
+
+if not exist "%NewScriptName%" (
+    rename "%ScriptName%" "%NewScriptName%"
+    color e4
+    echo Abra o arquivo sem adulterar os nomes
+    pause > Aperte qualquer tecla para sair do MathOptimizer EX:
+    exit
+) else (
+    goto criarinicializador
+)
+cls
+
+
+:adulteracionesilegaulis
+
+color e4
+echo Abra o arquivo sem adulterar os nomes
+pause
+
+
+
+:criarinicializador
+
+echo.
+if not exist "%StartupFolder%\%NewScriptName%" (
+    echo Feito por Luis
+    cls
+    copy "%NewScriptName%" "%StartupFolder%"
+    cls
+    goto start
+) else (
+    echo sem ideia nessa parte do code, mistake
+    cls
+    goto start
+)
+
+cls
+timeout /t 1 /nobreak >nul
+cls
+
+
+
+rem divisoria
+
+:start
+
+cls
+
+color 47
+echo Otimizador Automatico - Feito por Luis
+echo -------------------------------------------------------------------------------------
+echo.
+echo                                %date% , %time%
+echo.
+echo          ---              Iniciando a otimizacao automatica          ---              
+echo.
+echo         Para desativar, digite shell:startup e apague o arquivo (cl.bat)
+echo.
+echo.
+echo.
+echo -----------------------------------------------------------------------------------
+
+cleanmgr /sagerun:1
+
+del /q /s %temp%\*.* >nul
+
+del /q /s C:\Windows\Prefetch\*.* >nul
+
+del /q /s C:\Windows\Logs\*.* >nul
+
+rd /s /q C:\$Recycle.Bin >nul
+
+cls
+
+color a7
+echo Otimizador Automatico - Feito por Luis
+echo -------------------------------------------------------------------------------------
+echo.
+echo                                %date% , %time%
+echo.
+echo          ---              Limpeza automatica finalizada          ---              
+echo.
+echo         Para desativar, digite shell:startup e apague o arquivo (cl.bat)
+echo.
+echo.
+echo.
+echo -----------------------------------------------------------------------------------
+
+
+timeout -t 3 /nobreak >nul
+
+rem Aviso : Se o antivirus sinalizar esse arquivo como suspeito, remova-o, o código é aberto e você pode procurar na internet sobre como ele funciona / com o chatgpt.
+rem
+rem                                                              Feito por worbadill:tics#0
+rem
+rem - 15/11/2023
+rem
+rem Este arquivo serve para limpar automaticamente!
+

